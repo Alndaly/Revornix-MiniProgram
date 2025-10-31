6025f1e61b388630379288c0056333746885890a
Kinda Hall <1142704468@qq.com>
2025-10-31 23:14:45 +0800

    feat: 🎸 add 1173, support open chat materials via Mini Program

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
-onLaunch(() => {
-  console.log("App Launch");
+onLaunch((options) => {
+  console.log("App Launch", options);
+  if (options?.scene == 1173) {
+    uni.navigateTo({
+      url: `/pages/document/create/index?category=1&url=${options.forwardMaterials[0].path}`,
+    })
+  }
 });
 onShow(() => {
   console.log("App Show");
--- src/pages/document/create/index.vue
+++ src/pages/document/create/index.vue
@@ -2,4 +2,26 @@
     <div>
         document create page
     </div>
+    <div>
+        {{ category }}
+    </div>
+    <div>
+        {{ url }}
+    </div>
 </template>
+<script lang='ts' setup>
+import { onLoad } from '@dcloudio/uni-app';
+import { ref } from 'vue';
+
+const category = ref<number | null>(null)
+const url = ref<string | null>(null)
+
+onLoad(async (options) => {
+    if (options?.category) {
+        category.value = Number(options.category)
+    }
+    if (options?.url) {
+        url.value = options.url
+    }
+})
+</script>
