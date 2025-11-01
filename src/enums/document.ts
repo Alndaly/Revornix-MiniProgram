459113fb369d87945603dcd20d08dc7ff094c7ed
Kinda Hall <1142704468@qq.com>
2025-11-01 13:59:27 +0800

    feat: 🎸 improve the create page

--- src/enums/document.ts
+++ src/enums/document.ts
@@ -4,6 +4,19 @@ export enum DocumentCategory {
     QUICK_NOTE = 2,
 }
 
+export function getDocumentCategoryLabel(value: DocumentCategory | number): string {
+    switch (value) {
+        case DocumentCategory.FILE:
+            return "文件";
+        case DocumentCategory.WEBSITE:
+            return "网站";
+        case DocumentCategory.QUICK_NOTE:
+            return "速记";
+        default:
+            return "未知类型";
+    }
+}
+
 export enum DocumentMdConvertStatus {
     WAIT_TO = 0,
     CONVERTING = 1,
--- src/pages/document/create/index.vue
+++ src/pages/document/create/index.vue
@@ -1,15 +1,29 @@
 <template>
-    <div>
-        文档类型：
-        {{ category }}
+    <div class='container'>
+        <div class="form">
+            <div class='category'>
+                <div class='font-bold'>
+                    文档类型：
+                </div>
+                <div class='input text-sm'>
+                    {{ category && getDocumentCategoryLabel(category) }}
+                </div>
+            </div>
+            <div class='url'>
+                <div class='font-bold'>
+                    链接：
+                </div>
+                <div class='input text-sm'>
+                    {{ url }}
+                </div>
+            </div>
+        </div>
+        <button class='submit-button' @click="handleCreateDocument">创建文档</button>
     </div>
-    <div>
-        链接：
-        {{ url }}
-    </div>
-    <button @click="handleCreateDocument">创建文档</button>
+
 </template>
 <script lang="ts" setup>
+import { DocumentCategory, getDocumentCategoryLabel } from '@/enums/document';
 import { createDocument } from '@/service/document';
 import { onLoad } from '@dcloudio/uni-app';
 import { ref } from 'vue';
@@ -41,3 +55,55 @@ const handleCreateDocument = async () => {
     })
 }
 </script>
+
+<style lang='scss'>
+.text-sm {
+    font-size: 28rpx;
+}
+
+.input {
+    margin-top: 10rpx;
+    padding: 10rpx;
+    border: 1px solid #ccc;
+    border-radius: 10rpx;
+    background-color: #f4f4f4;
+}
+
+.font-bold {
+    font-weight: bold;
+}
+
+.container {
+    padding: 30rpx;
+    box-sizing: border-box;
+    word-break: break-all;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    padding-bottom: calc(env(safe-area-inset-bottom) + 10rpx);
+
+    .form {
+        flex: 1;
+
+        .category {
+            margin-bottom: 20rpx;
+        }
+
+        .url {
+            margin-bottom: 20rpx;
+        }
+
+    }
+
+    .submit-button {
+        width: 100%;
+        height: 80rpx;
+        line-height: 80rpx;
+        text-align: center;
+        background-color: #007aff;
+        color: #fff;
+        border-radius: 10rpx;
+        font-size: 32rpx;
+    }
+}
+</style>
