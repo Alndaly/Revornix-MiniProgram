cffddba9210a502644262229671869cbb7ad1838
Kinda Hall <1142704468@qq.com>
2025-11-02 21:35:43 +0800

    feat: 🎸 add some pages

---
+++ src/pages/document/unread/index.vue
@@ -0,0 +1,98 @@
+<template>
+    <view class='container bg-background'>
+        <scroll-view scroll-y class='document-list'>
+            <view v-for="(document, idx) in documents" :key="idx" class="document-wrapper">
+                <document-item :document="document" />
+            </view>
+        </scroll-view>
+    </view>
+</template>
+<script lang='ts' setup>
+import DocumentItem from '@/components/documentItem/index.vue';
+import type { DocumentInfo } from '@/generated';
+import { searchUserUnreadDocument } from '@/service/document';
+import { onLoad, onPullDownRefresh, onReachBottom } from '@dcloudio/uni-app';
+import { ref } from 'vue';
+import { useTabStore } from '@/store/tab'
+
+const tabStore = useTabStore()
+tabStore.switchTab(0)
+
+let start = ref<number | null>(null);
+let limit = ref(10);
+let keyword = ref('');
+let desc = ref(true);
+let has_more = ref(true);
+let documents = ref<DocumentInfo[]>([]);
+
+onLoad(async () => {
+    const data = await searchUserUnreadDocument({
+        start: start.value,
+        limit: limit.value,
+        keyword: keyword.value,
+        desc: desc.value
+    })
+
+    documents.value = data.elements
+    has_more.value = data.has_more
+    if (data.next_start) {
+        start.value = data.next_start
+    }
+})
+
+onPullDownRefresh(async () => {
+    console.log('下拉刷新')
+    start.value = null
+    has_more.value = true
+
+    const data = await searchUserUnreadDocument({
+        start: start.value,
+        limit: limit.value,
+        keyword: keyword.value,
+        desc: desc.value
+    })
+
+    documents.value = data.elements
+    has_more.value = data.has_more
+    if (data.next_start) {
+        start.value = data.next_start
+    }
+    uni.stopPullDownRefresh()
+})
+
+onReachBottom(async () => {
+    console.log('上拉触底')
+    if (!has_more.value) {
+        return
+    }
+    const data = await searchUserUnreadDocument({
+        start: start.value,
+        limit: limit.value,
+        keyword: keyword.value,
+        desc: desc.value
+    })
+
+    documents.value = documents.value.concat(data.elements)
+    has_more.value = data.has_more
+    if (data.next_start) {
+        start.value = data.next_start
+    }
+})
+
+</script>
+
+<style lang='scss'>
+.document-list {
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+
+    .document-wrapper {
+        margin-bottom: 20rpx;
+    }
+
+    .document-wrapper:last-child {
+        margin-bottom: 0;
+    }
+}
+</style>
---
+++ src/pages/section/subscribed/index.vue
@@ -0,0 +1,96 @@
+<template>
+    <view class='container bg-background'>
+        <scroll-view scroll-y class='section-list'>
+            <view v-for="(section, idx) in sections" :key="idx" class="section-wrapper">
+                <section-item :section="section" />
+            </view>
+        </scroll-view>
+    </view>
+</template>
+<script lang='ts' setup>
+import type { SectionInfo } from '@/generated';
+import { getMySubscribedSecitions } from '@/service/section';
+import { onLoad, onPullDownRefresh, onReachBottom } from '@dcloudio/uni-app';
+import { ref } from 'vue';
+import SectionItem from '@/components/sectionItem/index.vue'
+import { useTabStore } from '@/store/tab'
+
+const tabStore = useTabStore()
+tabStore.switchTab(1)
+
+let start = ref<number | null>(null);
+let limit = ref(10);
+let keyword = ref('');
+let desc = ref(true);
+let has_more = ref(true);
+let sections = ref<SectionInfo[]>([]);
+
+onLoad(async () => {
+    const data = await getMySubscribedSecitions({
+        start: start.value,
+        limit: limit.value,
+        keyword: keyword.value,
+        desc: desc.value
+    })
+
+    sections.value = data.elements
+    has_more.value = data.has_more
+    if (data.next_start) {
+        start.value = data.next_start
+    }
+})
+
+onPullDownRefresh(async () => {
+    console.log('下拉刷新')
+    start.value = null
+    has_more.value = true
+
+    const data = await getMySubscribedSecitions({
+        start: start.value,
+        limit: limit.value,
+        keyword: keyword.value,
+        desc: desc.value
+    })
+
+    sections.value = data.elements
+    has_more.value = data.has_more
+    if (data.next_start) {
+        start.value = data.next_start
+    }
+    uni.stopPullDownRefresh()
+})
+
+onReachBottom(async () => {
+    console.log('上拉触底')
+    if (!has_more.value) {
+        return
+    }
+    const data = await getMySubscribedSecitions({
+        start: start.value,
+        limit: limit.value,
+        keyword: keyword.value,
+        desc: desc.value
+    })
+
+    sections.value = sections.value.concat(data.elements)
+    has_more.value = data.has_more
+    if (data.next_start) {
+        start.value = data.next_start
+    }
+})
+</script>
+<style lang="scss">
+.section-list {
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+
+    .section-wrapper {
+        margin-bottom: 20rpx;
+    }
+
+    .section-wrapper:last-child {
+        margin-bottom: 0;
+    }
+}
+</style>
